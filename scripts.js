a653229be978e33112ee7f5a4a0bff191a5c331d
I1una <92636586+SarutobiMisumi@users.noreply.github.com>
2024-10-05 20:24:34 +0700

    new

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    const headerOffset = 60;  // Adjust this value based on your header height
+    const elementPosition = target.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
     });
   });
-  
+});
